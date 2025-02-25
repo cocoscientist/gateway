c6ece02b5eecedd57a1bdd5d090542551ddf3c7d
Yash <ypt7895@gmail.com>
2025-02-26 00:58:55 +0530

    base interception

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const {setupProxies} = require("./proxy")
 
 const app = express()
 
+app.use('/games',async (req,res,next)=>{
+    if(req.query.title=='portal'){
+        res.send('Intercepted and Returned')
+    }else{
+        next()
+    }
+})
+
 setupProxies(app, ROUTES)
 
 app.listen(8000,()=>{console.log('Listening on Port 8000')})
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const ROUTES = [
     {
         url: '/games',
         proxy: {
-            target: "https://www.cheapshark.com/api/1.0/games",
+            target: "https://www.cheapshark.com/api/1.0/",
             changeOrigin: true
         }
     }
